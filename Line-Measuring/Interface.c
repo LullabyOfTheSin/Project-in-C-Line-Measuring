27c929a43f92a3391554b0b400e8bbba22ae1fd9
LullabyOfTheSin <kamilmarko8@gmail.com>
2018-01-24 08:19:23 +0100

    Testing GTK image displaying function
    Works as intended, no problems so far

--- Line-Measuring/Interface.c
+++ Line-Measuring/Interface.c
@@ -3,11 +3,15 @@
 void InterfaceInit(int argc, char *argv[])
 {
     GtkWidget *okno;
+    GtkWidget *image;
 
     gtk_init (&argc, &argv);
 
-    okno = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show (okno);
+    okno=gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    image=gtk_image_new_from_file("abc.png");
+    gtk_container_add(GTK_CONTAINER(okno),image);
+    gtk_widget_show(image);
+    gtk_widget_show(okno);
 
     gtk_main ();
 }
